2046db70d738f8ec43e79d22a79c8a2937c248f9
Diogo Vilhena <diogodevilhena@sapo.pt>
2020-11-30 19:55:11 +0000

    Logo, and start of the desgin

--- app/src/main/java/pt/vilhena/listit/MainActivity.kt
+++ app/src/main/java/pt/vilhena/listit/MainActivity.kt
@@ -1,9 +1,10 @@
 package pt.vilhena.listit
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
